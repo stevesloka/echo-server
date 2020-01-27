61e37855e499a00e8f774a9ccd5b7aaa14b6eecd
Steve Sloka <slokas@vmware.com>
2020-01-27 14:20:07 -0500

    Add response delay flag
    Signed-off-by: Steve Sloka <slokas@vmware.com>

--- main.go
+++ main.go
@@ -4,24 +4,38 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"time"
 )
 
-	func getRequest(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(fmt.Sprintf("ECHO Request Server: \n--------------------\n")))
-		w.Write([]byte(fmt.Sprintf("App: \n    %s\n", echoText)))
+var (
+	echoText      string
+	responseDelay time.Duration
+	delay         <-chan time.Time
+)
 
-		headers := r.Header
-		w.Write([]byte(fmt.Sprintf("Request: \n    http://%s%s\n", r.Host, r.RequestURI)))
-		w.Write([]byte(fmt.Sprintf("Headers: \n    %s\n", headers)))
+func getRequest(w http.ResponseWriter, r *http.Request) {
+
+	// Add delay if enabled
+	if responseDelay > 0 {
+		<-delay
+	}
+
+	w.Write([]byte(fmt.Sprintf("ECHO Request Server: \n--------------------\n")))
+	w.Write([]byte(fmt.Sprintf("App: \n    %s\n", echoText)))
+
+	headers := r.Header
+	w.Write([]byte(fmt.Sprintf("Request: \n    http://%s%s\n", r.Host, r.RequestURI)))
+	w.Write([]byte(fmt.Sprintf("Headers: \n    %s\n", headers)))
 }
 
-var echoText string
 func init() {
 	flag.StringVar(&echoText, "echotext", "", "enter text to echo back to the user")
+	flag.DurationVar(&responseDelay, "response-delay", 0, "")
 }
 
 func main() {
 	flag.Parse()
+	delay = time.Tick(responseDelay)
 	http.HandleFunc("/", getRequest)
 	http.ListenAndServe(":8080", nil)
 }
